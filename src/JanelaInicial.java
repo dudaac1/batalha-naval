ce9e1b71ae002501fba2439748722100b72e2eb3
dudaac1 <dudaac99@gmail.com>
2021-06-03 13:55:59 -0300

    método de evento do botão iniciado

--- src/JanelaInicial.java
+++ src/JanelaInicial.java
@@ -1,35 +1,43 @@
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 /**
  * @author Eduarda e Julia
  */
-public class JanelaInicial extends JFrame {
+public class JanelaInicial extends JFrame implements ActionListener {
+    // globais para a classe pois serão necessários em outros pontos
     private JLabel labPreencha;
+    private JTextField fieldNome;
     private JRadioButton radioAleatorio;
     private JRadioButton radioMontar;
     private JButton btnJogar;
     
     public JanelaInicial() {
-        inicializar();
+      inicializar();
+        //run();
     }
     
-    public void inicializar() {
+    
+   public void inicializar() {
         // configurações da janela
-        this.setTitle("Bem-Vindo");
-        this.setSize(600, 450);
-        this.setLocationRelativeTo(null); // centraliza no monitor
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE); // matar o processo se apertar o X
-        this.setFont(new Font("Arial", Font.PLAIN, 14)); // config da fonte geral
-        this.setMinimumSize(new Dimension(600, 450)); // janela não pode ser menor que isso
+        setTitle("Bem-Vindo");
+        setSize(600, 450);
+        setLocationRelativeTo(null); // centraliza no monitor
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // matar o processo se apertar o X
+        setFont(new Font("Arial", Font.PLAIN, 14)); // config da fonte geral
+        setMinimumSize(new Dimension(600, 450)); // janela não pode ser menor que isso
+        setVisible(true);
         //this.setBackground(new Color(0, 0, 0)); // valores rgb
+        
+        Container janela = getContentPane();
+//        janela.setLayout(new GridLayout(0, 1));
         
         // configurações dos componentes
         JPanel painel = new JPanel();
-        painel.setLayout(new GridLayout(0, 1)); 
-        // setar as colunas como 0 significa que serão adicionadas conforme necessário
-        painel.setComponentOrientation(ComponentOrientation.UNKNOWN);
-        this.add(painel);
+        //painel.setLayout(null);
+        painel.setLayout(new GridLayout(0, 1)); // setar como 0 = serão adicionados quantos necessáerios
+        janela.add(painel);
         
         JLabel labTitulo = new JLabel("BATALHA NAVAL");
         labTitulo.setFont(new Font("Arial Black", Font.BOLD, 36));
@@ -42,8 +50,8 @@ public class JanelaInicial extends JFrame {
         JLabel labInsiraNome = new JLabel("Insira seu nome:");
         painel.add(labInsiraNome);
         
-        JTextField fieldName = new JTextField();
-        painel.add(fieldName);
+        fieldNome = new JTextField();
+        painel.add(fieldNome);
         
         // só vai ser usado se, quando o usuário apertar JOGAR
         // o campo do nome tiver vazio
@@ -57,23 +65,62 @@ public class JanelaInicial extends JFrame {
         labVcDeseja.setFont(new Font("Arial", Font.PLAIN, 18));
         painel.add(labVcDeseja);
         
-        JPanel painelRadio = new JPanel(); // para deixar os radio buttons lado a lado
-        painelRadio.setLayout(new GridLayout(1, 0));
         radioAleatorio = new JRadioButton("Gerar jogo aleatório");
         radioAleatorio.setSelected(true); // por padrão o tipo Jogo Aleatório vai ta selecionado
-        painelRadio.add(radioAleatorio);
+//        radioAleatorio.addActionListener(this);
+        
         radioMontar = new JRadioButton("Montar seu jogo");
-        radioMontar.setSelected(false); 
+//        radioMontar.addActionListener(this);
+        
+        ButtonGroup radioGroup = new ButtonGroup(); // para que só um radioButton esteja selecionado por vez
+        radioGroup.add(radioAleatorio);
+        radioGroup.add(radioMontar);
+        
+        JPanel painelRadio = new JPanel(); // para deixar os radioButtons juntos
+        painelRadio.setLayout(new GridLayout(1, 0)); // para ficarem lado a lado
+        painelRadio.add(radioAleatorio); // adicionando radioButtons ao painel
         painelRadio.add(radioMontar);
         painel.add(painelRadio);
         
         btnJogar = new JButton("Jogar");
+        btnJogar.setBounds(50, 30, 100, 30);
+//        btnJogar.setMaximumSize(new Dimension(150, 20));
+//        btnJogar.setMinimumSize(new Dimension(150, 20));
+//        btnJogar.setPreferredSize(new Dimension(150, 20));
+        
+        btnJogar.addActionListener(this); // para o evento do botão
         painel.add(btnJogar);
     }
     
+   @Override
+    public void actionPerformed(ActionEvent event) {  // método do evento do botão
+                //    private JTextField fieldName;
+                //    private JRadioButton radioAleatorio;
+                //    private JRadioButton radioMontar;
+                String nomeJogador = fieldNome.getText();
+                System.out.println("Este é o nome: "+nomeJogador);
+                System.out.println("Jogo aleatório está "+radioAleatorio.isSelected() );
+                System.out.println("Montar jogo está "+radioMontar.isSelected());
+                System.out.println();
+                
+                labPreencha.setVisible(true);
+            }
+           
+//    public static void main(String args[]) {
+//        JanelaInicial janelaInicial = new JanelaInicial();
+//        janelaInicial.setVisible(true);
+//   }
     
-    public static void main(String args[]) {
-        JanelaInicial janelaInicial = new JanelaInicial();
-        janelaInicial.setVisible(true);
+   public static void main(String[] args) {
+////        SwingUtilities.invokeLater(new JanelaInicial());
+        EventQueue.invokeLater(new Runnable() {
+//        
+            @Override
+            public void run() {
+                JanelaInicial janela = new JanelaInicial();
+               janela.setVisible(true);           
+            }
+
+        });
     }
 }
